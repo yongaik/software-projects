e51b44d685da3fe38341607fa11c02439404bdee
yongaik <yongaik@gmail.com>
2025-01-18 10:18:31 +0800

    added the count to ten code

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print("Hello World by Aik, from python")
 print("First Change.")
 print("Second Change added auto by docker compose.")
 print("third Change added auto by docker compose.")
+for counter in range(1,11):
+    print(counter)
