f97993aa43b2ca7f0dc9e635b9ab949d5d8876b8
yongaik <yongaik@gmail.com>
2025-01-18 11:53:26 +0800

    editing code from ubuntu g5dev-ubun11 after clone from github

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ print("Hello World by Aik, from python")
 print("First Change.")
 print("Second Change added auto by docker compose.")
 print("third Change added auto by docker compose.")
+print("four change added (on other ubuntu desktop  after git clone the remote repo")
 for counter in range(1,11):
     print(counter)
